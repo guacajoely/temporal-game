cd8033b96c0b266ae25b7789c4a94fb48b58fce2
dlprofitt <dlprofitt@gmail.com>
2025-05-04 19:02:10 -0400

    Added frontend vite react-js app

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Temporal Game</h1>
+      <p>Welcome to your Temporal Game application</p>
+    </div>
+  )
+}
+
+export default App
